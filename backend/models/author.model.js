b4aa39da8a39222e7a7d4e0ce25c51f4bdb5bf53
krishnaraketla <raketla.k@northeastern.edu>
2026-06-06 12:00:35 -0400

    Remove backend; frontend-only repo with external API

--- backend/models/author.model.js
+++
@@ -1,9 +0,0 @@
-const authorSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    birth_date: String,
-    death_date: String,
-    bio: String,
-    // Add other relevant fields
-});
-
-module.exports = mongoose.model('Author', authorSchema);
